3d7b6f5feca9784d470b802e95f6cb044f041011
tiptondt1998 <dylan.tipton@live.com>
2021-02-26 15:36:29 -0500

    updated idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -26,7 +26,7 @@ function saveRecord(record) {
 getAll.onsuccess = function() {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/pizzas', {
+      fetch('../../routes/api.js', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
@@ -40,13 +40,13 @@ getAll.onsuccess = function() {
             throw new Error(serverResponse);
           }
           // open one more transaction
-          const transaction = db.transaction(['new_pizza'], 'readwrite');
+          const transaction = db.transaction(['Transaction'], 'readwrite');
           // access the new_pizza object store
-          const pizzaObjectStore = transaction.objectStore('new_pizza');
+          const transactionObjectStore = transaction.objectStore('Transaction');
           // clear all items in your store
           pizzaObjectStore.clear();
 
-          alert('All saved pizza has been submitted!');
+          alert('All saved transactions has been submitted!');
         })
         .catch(err => {
           console.log(err);
